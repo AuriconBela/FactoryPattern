891108f23c9d33a31673198cdce39024b407d091
Balazs, Bela (ADV D EU HU OPS 2 MPS 8) <bela.balazs@evosoft.com>
2023-09-01 14:39:12 +0200

    Minor changes

--- Form1.cs
+++ Form1.cs
@@ -12,7 +12,6 @@ namespace SampleApp
         private async void button1_ClickAsync(object sender, EventArgs e)
         {
             var gepeszet = new Gepeszet();
-            //await gepeszet.CreateSampleDataAsync("Assets\\sampledataset.json");
 
             await gepeszet.LoadFromFile("Assets\\sampledataset.json");
         }
--- Model/Gepeszet.cs
+++ Model/Gepeszet.cs
@@ -27,98 +27,98 @@ public class Gepeszet
 		}
     }
 
-    public async Task<bool> SaveToFile(string fileName)
-    {
-        try
-        {
-            var data = JsonConvert.SerializeObject(_elemek, Formatting.Indented);
+    //public async Task<bool> SaveToFile(string fileName)
+    //{
+    //    try
+    //    {
+    //        var data = JsonConvert.SerializeObject(_elemek, Formatting.Indented);
 
-            if (data == null) { return false; }
+    //        if (data == null) { return false; }
 
-            await File.WriteAllTextAsync(fileName, data);
-            return true;
-        }
-        catch (Exception)
-        {
-            return false;
-            throw;
-        }
-    }
+    //        await File.WriteAllTextAsync(fileName, data);
+    //        return true;
+    //    }
+    //    catch (Exception)
+    //    {
+    //        return false;
+    //        throw;
+    //    }
+    //}
 
-    public async Task CreateSampleDataAsync(string fileName)
-    {
-        var futes = new Futes()
-        {
-            Name = "Fűtés",
-            Description = "A fűtési rendszer",
-            Image = Image.FromFile("c:\\Munka\\KnowHowSharing\\DesignPatterns\\Factory\\SampleApp\\Assets\\1.jfif")
-        };
+    //public async Task CreateSampleDataAsync(string fileName)
+    //{
+    //    var futes = new Futes()
+    //    {
+    //        Name = "Fűtés",
+    //        Description = "A fűtési rendszer",
+    //        Image = Image.FromFile("c:\\Munka\\KnowHowSharing\\DesignPatterns\\Factory\\SampleApp\\Assets\\1.jfif")
+    //    };
 
-        _elemek.Add(futes);
+    //    _elemek.Add(futes);
 
-        var hutes = new Hutes()
-        {
-            Name = "Hűtés",
-            Description = "A Hűtési rendszer",
-            Image = Image.FromFile("c:\\Munka\\KnowHowSharing\\DesignPatterns\\Factory\\SampleApp\\Assets\\2.jfif")
-        };
+    //    var hutes = new Hutes()
+    //    {
+    //        Name = "Hűtés",
+    //        Description = "A Hűtési rendszer",
+    //        Image = Image.FromFile("c:\\Munka\\KnowHowSharing\\DesignPatterns\\Factory\\SampleApp\\Assets\\2.jfif")
+    //    };
 
-        _elemek.Add(hutes);
+    //    _elemek.Add(hutes);
 
-        var hmv = new HasznalatiMelegViz()
-        {
-            Name = "Használati melegvíz",
-            Description = "A használati melegvíz",
-            Image = Image.FromFile("c:\\Munka\\KnowHowSharing\\DesignPatterns\\Factory\\SampleApp\\Assets\\3.jfif")
-        };
+    //    var hmv = new HasznalatiMelegViz()
+    //    {
+    //        Name = "Használati melegvíz",
+    //        Description = "A használati melegvíz",
+    //        Image = Image.FromFile("c:\\Munka\\KnowHowSharing\\DesignPatterns\\Factory\\SampleApp\\Assets\\3.jfif")
+    //    };
 
-        _elemek.Add(hmv);
+    //    _elemek.Add(hmv);
 
-        var szell = new Szellozes()
-        {
-            Name = "Szellőzési rendszer",
-            Description = "A szellőzési rendszer",
-            Image = Image.FromFile("c:\\Munka\\KnowHowSharing\\DesignPatterns\\Factory\\SampleApp\\Assets\\4.jfif")
-        };
+    //    var szell = new Szellozes()
+    //    {
+    //        Name = "Szellőzési rendszer",
+    //        Description = "A szellőzési rendszer",
+    //        Image = Image.FromFile("c:\\Munka\\KnowHowSharing\\DesignPatterns\\Factory\\SampleApp\\Assets\\4.jfif")
+    //    };
 
-        _elemek.Add(szell);
+    //    _elemek.Add(szell);
 
-        var vil = new Vilagitas()
-        {
-            Name = "Világítás",
-            Description = "A világítás leírása",
-            Image = Image.FromFile("c:\\Munka\\KnowHowSharing\\DesignPatterns\\Factory\\SampleApp\\Assets\\5.jfif")
-        };
+    //    var vil = new Vilagitas()
+    //    {
+    //        Name = "Világítás",
+    //        Description = "A világítás leírása",
+    //        Image = Image.FromFile("c:\\Munka\\KnowHowSharing\\DesignPatterns\\Factory\\SampleApp\\Assets\\5.jfif")
+    //    };
 
-        _elemek.Add(vil);
+    //    _elemek.Add(vil);
 
-        var ne = new Napelem()
-        {
-            Name = "Napelem",
-            Description = "A napelem leírása",
-            Image = Image.FromFile("c:\\Munka\\KnowHowSharing\\DesignPatterns\\Factory\\SampleApp\\Assets\\6.jfif")
-        };
+    //    var ne = new Napelem()
+    //    {
+    //        Name = "Napelem",
+    //        Description = "A napelem leírása",
+    //        Image = Image.FromFile("c:\\Munka\\KnowHowSharing\\DesignPatterns\\Factory\\SampleApp\\Assets\\6.jfif")
+    //    };
 
-        _elemek.Add(ne);
+    //    _elemek.Add(ne);
 
-        var nk = new NapKollektor()
-        {
-            Name = "NapKollektor",
-            Description = "A napKollektor leírása",
-            Image = Image.FromFile("c:\\Munka\\KnowHowSharing\\DesignPatterns\\Factory\\SampleApp\\Assets\\7.jfif")
-        };
+    //    var nk = new NapKollektor()
+    //    {
+    //        Name = "NapKollektor",
+    //        Description = "A napKollektor leírása",
+    //        Image = Image.FromFile("c:\\Munka\\KnowHowSharing\\DesignPatterns\\Factory\\SampleApp\\Assets\\7.jfif")
+    //    };
 
-        _elemek.Add(nk);
+    //    _elemek.Add(nk);
 
-        var em = new EgyebMegujulo()
-        {
-            Name = "Egyéb megújuló",
-            Description = "Az egyéb megújuló rendszer leírása",
-            Image = Image.FromFile("c:\\Munka\\KnowHowSharing\\DesignPatterns\\Factory\\SampleApp\\Assets\\8.jfif")
-        };
+    //    var em = new EgyebMegujulo()
+    //    {
+    //        Name = "Egyéb megújuló",
+    //        Description = "Az egyéb megújuló rendszer leírása",
+    //        Image = Image.FromFile("c:\\Munka\\KnowHowSharing\\DesignPatterns\\Factory\\SampleApp\\Assets\\8.jfif")
+    //    };
 
-        _elemek.Add(em);
+    //    _elemek.Add(em);
 
-        await SaveToFile(fileName);
-    }
+    //    await SaveToFile(fileName);
+    //}
 }
